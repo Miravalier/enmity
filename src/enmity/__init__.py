375989b07bca18c951a1f18c2ad175b7c6e46aac
Benjamin Miramontes <benjamin@miramontes.dev>
2021-10-10 21:36:35 -0400

    Version 0.1.0 - Interactions API

--- src/enmity/__init__.py
+++ src/enmity/__init__.py
@@ -2,6 +2,6 @@ __title__ = "enmity"
 __author__ = "Benjamin Miramontes"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Benjamin Miramontes"
-__version__ = "0.0.6"
+__version__ = "0.1.0"
 
 from .bot import Bot
